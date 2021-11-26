450365a50b2563fe0da3fdb0318c27ad957ffd47
Kristoffer Karlsson <kristoffer.karlsson@isotop.se>
2021-11-26 11:19:20 +0100

    owoooo
    Signed-off-by: Kristoffer Karlsson <kristoffer.karlsson@isotop.se>

--- lib/presentation/ui/pizza_card.dart
+++ lib/presentation/ui/pizza_card.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class PizzaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Text("im a pizza!!!!!!");
+    return const Text("im a pizza!!!!!!!");
   }
 }
